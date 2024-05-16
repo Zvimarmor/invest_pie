95ac5032870dd504f4f6c55ad507cbd6e9df8628
Zvi Marmor <zvimarmor@MacBook-Pro-sl-Zvi.local>
2024-05-16 14:24:21 +0300

    working a little bit more on the user class

--- user_invest.py
+++ user_invest.py
@@ -2,18 +2,33 @@ import random
 from investment_policies import *
 from invest_showing import *
 
-USER_COUNTER = 0
 
 class user:
     """
     Represents an individual investor with their name, account number, investment policy and investment portfolio.
     """
-    def __init__(self, name, age, account_number):
-        self.name = input("Enter your name: ")
-        self.account_number = USER_COUNTER + 1
-        USER_COUNTER += 1
-        self.portfolio = portfolio(self.name, investment_policies["Conservative"], self.account_number)
+    def __init__(self, name, investment_policy, user_counter):
+        """
+        Initializes the user with their name, account number, investment policy, and portfolio.
+        """
+        self.name = name
+        self.investment_policy = investment_policy
+        self.account_number = user_counter + 1 
+        user_counter += 1
+        self.portfolio = portfolio(self.name, self.investment_policy, self.account_number)
 
-        
 
-        
+
+    
+if __name__ == "__main__":
+    user_counter = 0
+    user_name = input("Enter your name: ")
+    print("Available investment policies are: ")
+    for i in investment_policies:
+        print(i)
+    user_investment_policy = input("Enter the name of the investment policy you want to use: ")
+    if user_investment_policy not in investment_policies:
+        print("Investment policy not found")
+        exit()
+    user = user(user_name, investment_policies[user_investment_policy], user_counter)
+    print("Welcome " + user.name + "! Your account number is: " + str(user.account_number))
